00d7fa1c0c557a36c2ae95fd4d337de45012c1ec
Araceli Ramos <celi2455@gmail.com>
2021-11-02 13:48:07 -0500

    Completed assignment

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
 def my_find(collection)
+    i = 0
+    while i < collection.length
+        if yield(collection[i])
+            return collection[i] #can also refactor this slightly by putting if statement all on one line - return collection[i] if yield(collection[i])
+        end
+        i += 1
+    end
 
 end
